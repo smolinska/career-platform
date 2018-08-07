4985f43f79b99d4b53212f91d97c6c9b62122d19
Paulina Smolińska <smolinska.ps@gmail.com>
2018-08-07 16:47:59 +0200

    add footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
     path: 'home', component: HomeComponent
   },
   {
-    path: 'job', component: JobComponent
+    path: 'jobs', component: JobComponent
   },
 ];
 
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {LayoutComponent} from './layout.component';
 import {RouterModule} from '@angular/router';
+import {FooterComponent} from '../footer/footer.component';
 
 @NgModule({
   imports: [
@@ -13,6 +14,7 @@ import {RouterModule} from '@angular/router';
   ],
   declarations: [
     LayoutComponent,
+    FooterComponent,
   ]
 })
 export class LayoutModule {
